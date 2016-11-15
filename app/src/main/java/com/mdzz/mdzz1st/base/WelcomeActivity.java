e516f74f5a1a7777b684b9ab5c8744dc9e3f3fc8
SKY-20160523ZQP\admin <571129524@qq.com>
2016-11-15 11:21:26 +0800

    做好welcome动画

--- app/src/main/java/com/mdzz/mdzz1st/base/WelcomeActivity.java
+++ app/src/main/java/com/mdzz/mdzz1st/base/WelcomeActivity.java
@@ -1,12 +1,16 @@
 package com.mdzz.mdzz1st.base;
 
+import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
 
+import com.mdzz.mdzz1st.MainActivity;
 import com.mdzz.mdzz1st.R;
 
 /**
@@ -17,9 +21,12 @@ import com.mdzz.mdzz1st.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private static final String TAG = "AppCompatActivity";
+
     private ImageView mImageViewDictionaryIcon;
     private ImageView mImageViewIdiomIcon;
     private ImageView mImageViewTransitionIcon;
+
     private int mScreenWidth;
     private int mScreenHeight;
     private int mScreenCenterX;
@@ -43,32 +50,70 @@ public class WelcomeActivity extends AppCompatActivity {
         mScreenHeight = getWindowManager().getDefaultDisplay().getHeight(); // 屏幕高
         mScreenCenterX = mScreenWidth / 2;
         mScreenCenterY = mScreenHeight / 2;
+
+        Log.v(TAG, "1:" + mScreenCenterX + "   2:" + mScreenCenterY);
     }
 
     private void setAnimation() {
         ObjectAnimator translationAnimator1 = ObjectAnimator.ofFloat(
-                mImageViewDictionaryIcon, "translationY",
-                mScreenCenterX, mScreenCenterY, mScreenCenterX, mScreenCenterY - mScreenCenterY / 2);
+                mImageViewDictionaryIcon, "translationY", -mScreenCenterY / 2);
         translationAnimator1.setInterpolator(new AccelerateDecelerateInterpolator());
         ObjectAnimator translationAnimator2 = ObjectAnimator.ofFloat(
-                mImageViewTransitionIcon, "translationY",
-                mScreenCenterX, mScreenCenterY, mScreenCenterX, mScreenCenterY + mScreenCenterY / 2);
+                mImageViewTransitionIcon, "translationY", mScreenCenterY / 2);
         translationAnimator2.setInterpolator(new AccelerateDecelerateInterpolator());
 
         ObjectAnimator rotationAnimator1 = ObjectAnimator.ofFloat(
                 mImageViewDictionaryIcon, "rotation", 0, 360, 0);
         ObjectAnimator rotationAnimator2 = ObjectAnimator.ofFloat(
                 mImageViewTransitionIcon, "rotation", 0, 360, 0);
+        ObjectAnimator rotationAnimator3 = ObjectAnimator.ofFloat(
+                mImageViewIdiomIcon, "rotation", 0, 360, 0);
+        rotationAnimator3.setDuration(3000);
 
         AnimatorSet set1 = new AnimatorSet();
         // set1.playSequentially(translationAnimator1, rotationAnimator1); // 顺序播放
         set1.playTogether(translationAnimator1, rotationAnimator1); // 一起播放
         set1.setDuration(3000);
         AnimatorSet set2 = new AnimatorSet();
-        set2.playTogether(translationAnimator2, rotationAnimator2); // 一起播放
+        set2.playTogether(translationAnimator2, rotationAnimator2);
         set2.setDuration(3000);
 
         set1.start();
         set2.start();
+        rotationAnimator3.start();
+
+        rotationAnimator3.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(1500);
+                            Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/mdzz/mdzz1st/util/DensityUtil.java
@@ -0,0 +1,28 @@
+package com.mdzz.mdzz1st.util;
+
+import android.content.Context;
+
+/**
+ * @author 小侨
+ * @time 2016/11/15  10:37
+ * @desc ${TODD}
+ */
+
+public class DensityUtil {
+
+    /**
+     * 根据手机的分辨率从 dip 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp==dip
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
